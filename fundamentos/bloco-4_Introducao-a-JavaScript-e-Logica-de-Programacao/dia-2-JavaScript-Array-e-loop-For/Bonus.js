8426d9e849b8da2c845f65a81e61c7ddef5a285c
Vitor Brito <vitor.fernandes.brito@gmail.com>
2022-06-23 14:21:39 -0300

    Finalizado o exercicio bonus

--- fundamentos/bloco-4_Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-JavaScript-Array-e-loop-For/Bonus.js
+++ fundamentos/bloco-4_Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-JavaScript-Array-e-loop-For/Bonus.js
@@ -10,4 +10,28 @@ for (let index = 1;index < numbers.length; index++){
         }
     }
 }
-console.log(numbers)
+console.log(numbers);
+for (let index = (numbers.length-2);index >= 0; index--){
+    for(indexDois = (numbers.length-1); indexDois > index; indexDois--){
+        if (numbers[index] < numbers[indexDois]){
+            let valor = numbers[index];
+            numbers[index] = numbers[indexDois]
+            numbers[indexDois] = valor;
+        }
+    }
+}
+console.log(numbers);
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+newNumbers = [];
+for (let index = 0;index < numbers.length; index++){
+    let auxiIndex = index+1
+    if (auxiIndex > (numbers.length-1)){
+        let mult = numbers[index]*2
+        newNumbers.push(mult); 
+    }else{
+        newNumbers.push((numbers[index]*numbers[auxiIndex]));
+    }
+}
+console.log("ultima etapa")
+console.log(numbers);
+console.log(newNumbers);

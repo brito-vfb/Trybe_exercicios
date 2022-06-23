e03969b8f8bcd2ebc1908eba609e7ca00407de9c
Vitor Brito <vitor.fernandes.brito@gmail.com>
2022-06-23 13:58:32 -0300

    ordenar do menor parao maior

--- fundamentos/bloco-4_Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-JavaScript-Array-e-loop-For/Bonus.js
+++ fundamentos/bloco-4_Introducao-a-JavaScript-e-Logica-de-Programacao/dia-2-JavaScript-Array-e-loop-For/Bonus.js
@@ -2,11 +2,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Ordenar o array em ordem Crescente
 for (let index = 1;index < numbers.length; index++){
-    for(indexDois =0; indexDois < index; indexDois){
+    for(indexDois = 0; indexDois < index; indexDois++){
         if (numbers[index] < numbers[indexDois]){
             let valor = numbers[index];
-            numbers[index = numbers[indexDois]]
-            numbers[index] = valor;
+            numbers[index] = numbers[indexDois]
+            numbers[indexDois] = valor;
         }
     }
 }

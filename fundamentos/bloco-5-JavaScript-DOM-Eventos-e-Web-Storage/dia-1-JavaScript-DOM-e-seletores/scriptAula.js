8dcca86b52b1be86f99a326e9ef93b7da85fa24a
Vitor Brito <vitor.fernandes.brito@gmail.com>
2022-06-29 14:27:34 -0300

    exercicios de aula

---
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/dia-1-JavaScript-DOM-e-seletores/scriptAula.js
@@ -0,0 +1,5 @@
+let title = document.getElementById("page-title");
+title.innerText = "Inter Estelar";
+let secondPara = document.getElementById("second-paragraph");
+secondPara.innerText = "é um ótimo filme de ficção e fantasia"
+secondPara.style.backgroundColor = "blue"

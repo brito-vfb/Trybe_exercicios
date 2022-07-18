578517c49af1826fad1e60e81ca55861b00d3429
Vitor Brito <vitor.fernandes.brito@gmail.com>
2022-07-18 20:07:44 -0300

    exercicio 4 até o requisito 3

--- fundamentos/bloco-7-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/sum.test.js
+++ fundamentos/bloco-7-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/sum.test.js
@@ -1,6 +1,66 @@
 const {expect} = require('expect');
-const {sum, min} = require('./testJest');
+const {sum, min, myRemove, myFizzBuzz, encode, decode} = require('./testJest');
 
-test('verifica afuncao que veio', () =>{ 
-    expect(sum(2, 1)).toBe(3);
+test('Testa de 4 + 5 = 9', () =>{ 
+    expect(sum(4, 5)).toBe(9);
+});
+
+test('Testa de 0 + 0 = 0', () =>{ 
+    expect(sum(0, 0)).toBe(0);
+});
+
+test('verifica se dá um trow', () =>{ 
+    expect(() => {sum(4, '5')}).toThrow();
+});
+
+test('verifica se dá um trow com a menssagem', () =>{ 
+    expect(() => {sum(4, '5')}).toThrowError('parameters must be numbers');
+});
+
+test('verifica se a função remove o 3 do array [1, 2, 3, 4]', () =>{ 
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+});
+
+test('verifica se a função não retorna o array  [1, 2, 3, 4]', () =>{ 
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+});
+
+test('verifica se a função remove o 5 do array [1, 2, 3, 4]', () =>{ 
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+});
+
+test('Testa se o número 15 e divisivel por 5 e 3 e a funcao retorna fizzbuzz', () =>{ 
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+});
+
+test('Testa se o número 3 e retorna fizz', () =>{ 
+    expect(myFizzBuzz(3)).toEqual('fizz');
+});
+
+test('Testa se o número 5 retorna buzz', () =>{ 
+    expect(myFizzBuzz(5)).toEqual('buzz');
+});
+
+test('Testa se o número 7 retorna a si próprio', () =>{ 
+    expect(myFizzBuzz(7)).toEqual(7);
+});
+
+test('Testa se a string oi retorna false', () =>{ 
+    expect(myFizzBuzz('oi')).toBeFalsy();
+});
+
+test('Testa se encode é uma funcao', () =>{ 
+    expect(typeof encode).toBe('function');
+});
+
+test('Testa se decode é uma funcao', () =>{ 
+    expect(typeof decode).toBe('function');
+});
+
+test('Testa se encode retorna 1, 2, 3, 4, 5 quando recebe a, e, i, o, u', () =>{ 
+    expect(encode('a, e, i, o, u')).toEqual('1, 2, 3, 4, 5');
+});
+
+test('Testa se decode retorna a, e, i, o, u quando recebe 1, 2, 3, 4, 5', () =>{ 
+    expect(decode('1, 2, 3, 4, 5')).toEqual('a, e, i, o, u');
 });
--- fundamentos/bloco-7-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/testJest.js
+++ fundamentos/bloco-7-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/dia-3-Primeiros-passos-em-Jest/testJest.js
@@ -1,9 +1,76 @@
-const sum = (a,b) => {
-    return a+b;
-}
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new Error('parameters must be numbers');
+    }
+  
+    return a + b;
+};
 
 const min = (a,b) => {
     return a-b;
+};
+
+function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+        if (item !== arr[index]) {
+        newArr.push(arr[index]);
+        }
+    }
+    return newArr;
+};
+
+function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
 }
 
-module.exports = {sum, min};
+function encodeSistem(vogal) {
+    let letter = vogal;
+    let codeList = [{ vogal: 'a', number: '1' }, { vogal: 'e', number: '2' },
+      { vogal: 'i', number: '3' }, { vogal: 'o', number: '4' }, { vogal: 'u', number: '5' }];
+    for (let code of codeList) {
+      if (code.vogal === vogal) {
+        letter = code.number;
+      }
+    }
+    return letter;
+}
+function decodeSistem(vogal) {
+    let letter = vogal;
+    let codeList = [{ vogal: 'a', number: '1' }, { vogal: 'e', number: '2' },
+      { vogal: 'i', number: '3' }, { vogal: 'o', number: '4' }, { vogal: 'u', number: '5' }];
+    for (let code of codeList) {
+      if (code.number === vogal) {
+        letter = code.vogal;
+      }
+    }
+    return letter;
+}
+  
+function encode(sentence) {
+    // seu código aqui
+    let arraySentence = sentence.split('');
+    for (let index = 0; index < arraySentence.length; index += 1) {
+      arraySentence[index] = encodeSistem(arraySentence[index]);
+    }
+    return arraySentence.join('');
+}
+function decode(dSentence) {
+    // seu código aqui
+    let arraySentence = dSentence.split('');
+    for (let index = 0; index < arraySentence.length; index += 1) {
+      arraySentence[index] = decodeSistem(arraySentence[index]);
+    }
+    return arraySentence.join('');
+}
+
+  
+  // implemente seus testes aqui
+  
+  // implemente seus testes aqui
+
+module.exports = {sum, min, myRemove, myFizzBuzz, encode, decode};
